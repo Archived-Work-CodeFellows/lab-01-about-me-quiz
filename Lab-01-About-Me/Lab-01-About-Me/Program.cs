d8bb5a3b6ef7ae4c6aaf35002f37f3affef93622
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-05 11:32:51 -0700

    Added the stretch goal score counter

--- Lab-01-About-Me/Lab-01-About-Me/Program.cs
+++ Lab-01-About-Me/Lab-01-About-Me/Program.cs
@@ -8,12 +8,15 @@ namespace Lab_01_About_Me
         {
             //Stores the user name globally to be passed into other methods
             string user = UserName();
-            FirstQuestion(user);
-            SecondQuestion(user);
-            ThirdQuestion(user);
-            FourthQuestion(user);
-            FifthQuestion(user);
-            Ending(user);
+
+            //Here I call my ScoreCalculator and use the 
+            //question methods as arguments to calculate the 
+            //score at the end
+            ScoreCalculator(FirstQuestion(user),
+                SecondQuestion(user),
+                ThirdQuestion(user),
+                FourthQuestion(user),
+                FifthQuestion(user), user);
 
         }
         //This method is to get the users name to have a more
@@ -58,7 +61,7 @@ namespace Lab_01_About_Me
         }
         //This is a method that asks the user to pick from a given
         //list and then provides a response
-        static void SecondQuestion(string user)
+        static string SecondQuestion(string user)
         {
             Console.Clear();
             Console.WriteLine($"Alright {user}, Out of these instruments, what do I play? (select a number)");
@@ -76,9 +79,11 @@ namespace Lab_01_About_Me
             Console.Clear();
 
             Console.WriteLine($"Alright {user}, that was kind of a freebie.");
+            Console.WriteLine(" ");
             Console.WriteLine("I originally went to become a Bassoonist and Jazz Saxophonist, but I picked up");
             Console.WriteLine("Multiple instruments along the way. Yes laptop is an instrument too!");
             Console.ReadLine();
+            return "3";
         }
         //This method returns an integer based on user input. It also
         //has another nested method that since it was starting to become
@@ -122,7 +127,7 @@ namespace Lab_01_About_Me
         //Similar to the method for the second question
         //This provides the user with a list to select from
         //However this method has a correct answer
-        static void FourthQuestion(string user)
+        static string FourthQuestion(string user)
         {
             Console.Clear();
             Console.WriteLine($"Serious question time {user}, What is my favorite food? (select a number)");
@@ -142,18 +147,21 @@ namespace Lab_01_About_Me
                 Console.WriteLine("Oh man, you got me. I don't think I can possibly turn down ice cream.");
                 Console.WriteLine("Which sucks because I became lactose intolerant a few years back");
                 Console.WriteLine("But sometimes suffering makes you stronger");
+                Console.ReadLine();
+                return result;
             } else
             {
                 Console.WriteLine($"Ah that is a good choice {user} but sadly no");
                 Console.WriteLine("My heart belongs to Ice Cream.");
                 Console.WriteLine("Though I became lactose intolerant a while back");
                 Console.WriteLine("So it is a one-sided relationship");
+                Console.ReadLine();
+                return result;
             }
-            Console.ReadLine();
         }
         //This method is similar to the second and fourth questions
         //A list is provided to the user to select from.
-        static void FifthQuestion(string user)
+        static string FifthQuestion(string user)
         {
             Console.Clear();
             Console.WriteLine($"{user} this is the last question!");
@@ -176,14 +184,17 @@ namespace Lab_01_About_Me
                 Console.WriteLine("though just programming and problem solving are fun,");
                 Console.WriteLine("I am interested in creating unique worlds and technologies that have an impactful experience");
                 Console.WriteLine("for others. I have done everything above either in the past or still do so now.");
+                Console.ReadLine();
+                return result;
             }
             else
             {
                 Console.WriteLine("I have done all of these things either in the past or still continue to do so");
                 Console.WriteLine("but my biggest goal after Code Fellows is to get into game design");
                 Console.WriteLine("and that is where my motivation and passion are fixiated");
+                Console.ReadLine();
+                return result;
             }
-            Console.ReadLine();
         }
         //This method is to inform the user that they have reached the end
         //and that the application will be finished
@@ -192,6 +203,34 @@ namespace Lab_01_About_Me
             Console.Clear();
             Console.WriteLine($"Congrats {user}! You've made it to the end of this Quiz!");
             Console.WriteLine("I hoped you learned some new things about me!");
+            Console.WriteLine("Now lets check your score!");
+            Console.ReadLine();
+        }
+
+        //This method takes the question methods in as arguments
+        //and uses the return value from the user to check for
+        //scoring
+        static void ScoreCalculator(bool firstQ, string secondQ, int thirdQ, string fourthQ, string fifthQ, string user)
+        {
+            Console.Clear();
+            int counter = 0;
+
+            //Since the first question is a boolean return
+            //We can use a ternary to check if it is true.
+            //If it is, then it will  set counter to one, else stay 0
+            counter = firstQ ? 1 : 0;
+
+            if (secondQ == "3") counter++;
+            if (thirdQ == 1) counter++;
+            if (fourthQ == "2") counter++;
+            if (fifthQ == "4") counter++;
+
+            Ending(user);
+            Console.Clear();
+
+            Console.WriteLine($"After all is said and done, {user}, you got {counter} out of 5!");
+            Console.WriteLine(" ");
+            Console.WriteLine("Congrats!");
             Console.WriteLine("Bye-bye!");
             Console.ReadLine();
         }
